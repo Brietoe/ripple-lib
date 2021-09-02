474cfb299cd2cb695061a207f266c5b38e37786a
Jackson Mills <jmills@ripple.com>
2021-09-02 13:00:55 -0700

    Change wallet.ts to simplify export

--- test/integration/wallet.ts
+++ test/integration/wallet.ts
@@ -1,14 +1,2 @@
-'use strict';
-
-function getAddress() {
-  return 'rQDhz2ZNXmhxzCYwxU6qAbdxsHA4HV45Y2';
-}
-
-function getSecret() {
-  return 'shK6YXzwYfnFVn3YZSaMh5zuAddKx';
-}
-
-module.exports = {
-  getAddress,
-  getSecret
-};
+export const getAddress = "rQDhz2ZNXmhxzCYwxU6qAbdxsHA4HV45Y2";
+export const getSecret = "shK6YXzwYfnFVn3YZSaMh5zuAddKx";
